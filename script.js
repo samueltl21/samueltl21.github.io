25bb612a4981f15a9ff2658db34d2015c6c2be43
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2024-07-13 15:32:29 +0800

    fix the double click on the detail-exp

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function myFunction() {
     var x = document.getElementById("venture");
-    var y = document.getElementById("thunder")
+    var y = document.getElementById("thunder");
+    
     if (x.style.display === "none") {
       x.style.display = "block";
-      y.style.display="none";
+      y.style.display = "none";
     } else {
       x.style.display = "none";
-      y.style.display="block";
+      y.style.display = "block";
     }
   }
