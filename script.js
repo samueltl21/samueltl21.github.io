98093dabfb1845c35d9677b50b9f41487a694a4e
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2024-07-17 10:07:27 +0800

    finish engtech javascript function with jobdesc content

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-function myFunction(element1, element2) {
-    var x = document.getElementById("element2");
-    var y = document.getElementById("element1");
+function myFunction(idx) {
+    var t = document.getElementById("thunder");
+    var el1 = document.getElementById("element1");
+    var el2 = document.getElementById("element2");
+    var el3 = document.getElementById("element3");
+    var el4 = document.getElementById("element4");
+    var el5 = document.getElementById("element5");
     
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.style.display = "none";
+    if (idx==1) {
+      t.style.display = "none";
+      el1.style.display = "block";
+      el2.style.display = "none";
+      el3.style.display = "none";
+      el4.style.display = "none";
+      el5.style.display = "none";
+    } else if (idx==2) {
+      t.style.display = "none";
+      el1.style.display = "none";
+      el2.style.display = "block";
+      el3.style.display = "none";
+      el4.style.display = "none";
+      el5.style.display = "none";
+    } else if (idx==3) {
+      t.style.display = "none";
+      el1.style.display = "none";
+      el2.style.display = "none";
+      el3.style.display = "block";
+      el4.style.display = "none";
+      el5.style.display = "none";
+    } else if (idx==4) {
+      t.style.display = "none";
+      el1.style.display = "none";
+      el2.style.display = "none";
+      el3.style.display = "none";
+      el4.style.display = "block";
+      el5.style.display = "none";
+    } else if (idx==5) {
+      t.style.display = "none";
+      el1.style.display = "none";
+      el2.style.display = "none";
+      el3.style.display = "none";
+      el4.style.display = "none";
+      el5.style.display = "block";
     } else {
-      x.style.display = "none";
-      y.style.display = "block";
+      t.style.display = "block";
+      el1.style.display = "none";
+      el2.style.display = "none";
+      el3.style.display = "none";
+      el4.style.display = "none";
+      el5.style.display = "none";
     }
-  }
+
+}
